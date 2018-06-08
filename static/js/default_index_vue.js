35e4911dc097a399a9a7df64eff2c5939d899a6c
Alexandre Smirnov <sashasmart@gmail.com>
2018-06-07 22:03:40 -0700

    add filtering func to recipes

--- static/js/default_index_vue.js
+++ static/js/default_index_vue.js
@@ -27,6 +27,35 @@ var app = function() {
         );
     };
 
+    self.func.toggle_tag = function(tag_index){
+        console.log("toggle tag", tag_index);
+
+
+        self.vue.tags[tag_index].active = !self.vue.tags[tag_index].active;
+
+        self.func.update_filtered_recipes();
+
+    };
+
+    self.func.update_filtered_recipes = function(){
+        // TODO: optimize this
+        self.vue.filtered_recipes = [];
+
+        for(var i = 0; i < self.vue.recipes.length; i++){
+            var recipe = self.vue.recipes[i];
+            console.log("recipe: ", recipe);
+            for(var j = 0; j < self.vue.tags.length; j++){
+                var tag = self.vue.tags[j];
+                if(tag.active && recipe.tags.includes(tag.id)){
+                    self.vue.filtered_recipes.push(recipe);
+                    break;
+                }
+            }
+        }
+
+        console.log(self.vue.filtered_recipes);
+    };
+
     // Complete as needed.
     self.vue = new Vue({
         el: "#vue-div",
@@ -35,21 +64,48 @@ var app = function() {
         data: {
             recipes: [
                 {
+                    id: 1,
                     name: "name", 
                     description: "description",
-                    id: 1
+                    tags: [1, 3],
                 },
                 {
+                    id: 2,
                     name: "name2", 
                     description: "description2",
-                    id: 2
+                    tags: [2, 3],
+                },
+                {
+                    id: 2,
+                    name: "name2", 
+                    description: "description2",
+                    tags: [1],
+                },
+            ],
+            filtered_recipes: [],
+            tags: [
+                {
+                    id: 1,
+                    name: "Vegetarian",
+                    active: true,
+                },
+                {
+                    id: 2,
+                    name: "Keto",
+                    active: true,
+                },
+                {
+                    id: 3,
+                    name: "Vegan",
+                    active: true,
                 },
             ],
         },
         methods: self.func
     });
 
-    self.func.data.get_recipes();
+    //self.func.data.get_recipes();
+    self.func.update_filtered_recipes();
 
     return self;
 };
